18cf7ac79a4fd08a5832b84b1380b9f666aa658e
henryzord <henry.cagnini@gmail.com>
2024-02-09 09:12:01 -0300

    fixed requirements and added more slides

--- atividades/classes/exemplo_1.py
+++ atividades/classes/exemplo_1.py
@@ -1,20 +1,33 @@
-
-class Objeto:
-    def __init__(self, largura, altura, comprimento, peso):
-        """Construtor da classe Objeto"""
-        self.largura = largura
-        self.altura = altura
-        self.comprimento = comprimento
-        self.peso = peso
-
-    def __str__(self):
-        return f'Peso: {self.peso}' + \
-            f'A/L/C: {self.altura}/{self.largura}/{self.comprimento}'
+# precisa dizer que está herdando a classe object
+class Pessoa(object):  
+    def __init__(self, nome):
+        self.nome = nome
 
 
-class Animal(Objeto):
-    pass
+class Pai(Pessoa):
+    def __init__(self, nome, sobrenome):
+        super().__init__(nome)
+        self.sobrenome = sobrenome
 
 
-caixa = Objeto(largura=10, altura=5, comprimento=3, peso=5)
-print(caixa)
+class Mae(Pessoa):
+    def __init__(self, nome, sobrenome):
+        super().__init__(nome)
+        self.sobrenome = sobrenome
+
+
+class Filha(Pai, Mae):
+    def __init__(self, nome, sobrenome):
+        super().__init__(nome, sobrenome)
+
+
+def main():
+    p = Pai('João', 'da Silva')
+    m = Mae('Maria', 'Gonçalves')
+    f = Filha('Júlia', p.sobrenome)
+
+
+if __name__ == '__main__':
+    main()
+
+
